ba25d052b80142b0c348391f322aada80095beaa
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-11 19:21:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,10 @@
 # My Code here....
-def map_to_negativize(source_array) 
+def map_to_negativize(source_array)
+  index = 0 
+  result = []
+  while index < source_array.length do
+    result[index] = (source_array[index] * -1)
+    index += 1 
+  end
+  result
+end
